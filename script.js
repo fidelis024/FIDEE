e0797b67a62ea622a869262490d367878e810da1
Ezeh fidelis <Kinglegacy965@gmail.com>
2024-09-19 11:17:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleAside() {
+    var aside = document.getElementById("aside");
+    if (aside.style.display === "none") {
+      aside.style.display = "block";
+    } else {
+      aside.style.display = "none";
+    }
+  }
+  
+  
